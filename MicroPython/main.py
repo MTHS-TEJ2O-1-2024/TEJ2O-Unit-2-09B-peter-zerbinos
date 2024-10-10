4fd2123a16d136048988c71bcd60771d0b923314
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-10 15:26:57 +0000

    made code for rock paper scissors (NOT DONE 2)

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,33 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Peter Zerbinos
+Created on: Oct 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import sleep
+import random
 
+# variables
+random_number = -1
+score_number = 0
 
-display.scroll("Hello, World!")
+# rock paper scissors after shake
+while True:
+    if accelerometer.was_gesture("shake"):
+        random_number = random.randint(0, 2)
+
+    # if randomNumber was 0
+    if random_number == 0:
+        display.clear()
+        display.show_leds(
+            """
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        """
+        )
+        sleep(5)
+    
